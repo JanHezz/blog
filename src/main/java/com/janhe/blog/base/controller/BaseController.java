84d65260f1479bf9651ab0e52ee0bc82e81fe0cb
kangninghe <975532442@qq.com>
2019-03-15 17:35:04 +0800

    hahah

---
+++ src/main/java/com/janhe/blog/base/controller/BaseController.java
@@ -0,0 +1,19 @@
+package com.janhe.blog.base.controller;
+
+
+
+
+/**
+ * @CLASSNAME BaseController
+ * @Description 基础控制器
+ * @Auther Jan
+ * @DATE 2019/3/14 0014 21:36
+ */
+
+public abstract class BaseController {
+
+
+
+
+
+}
---
+++ src/main/java/com/janhe/blog/base/utils/JsonUtils.java
@@ -0,0 +1,15 @@
+package com.janhe.blog.base.utils;
+
+/**
+ * @author JanHe 简
+ * @version V1.0
+ * @Title:
+ * @Package
+ * @Description: TODO
+ * @date
+ */
+public class JsonUtils {
+
+
+
+}
